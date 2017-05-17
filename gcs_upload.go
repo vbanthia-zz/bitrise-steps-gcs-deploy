818981876a4e984d4c139ae1e5fcf28cee999373
Vishal Banthia <vishal.banthia.vb@gmail.com>
2017-05-16 20:51:40 -0700

    download keyfile

--- gcs_upload.go
+++ gcs_upload.go
@@ -1,39 +1,49 @@
-// package main
-//
-// import (
-//   "fmt"
-//   "log"
-//   "cloud.google.com/go/storage"
-//   "golang.org/x/net/context"
-// )
-//
-// func main() {
-//   ctx := context.Background()
-//
-//   projectId := "fury-panda"
-//
-//   client, err := storage.NewClient(ctx)
-//   if err != nil {
-//     log.FatalF("Failed to create client: %v", err)
-//   }
-//
-//   bucketName := "android-double-dev-apk"
-//
-//
-// }
-
 package main
 
 import (
 	"fmt"
 	"log"
 	"os"
+	"net/http"
+	"path/filepath"
 
 	"google.golang.org/api/storage/v1"
 
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
+
+	"github.com/bitrise-io/go-utils/pathutil"
 )
+
+func downloadFile(downloadURL, targetPath string) error {
+	outFile, err := os.Create(targetPath)
+	if err != nil {
+		return fmt.Errorf("failed to create (%s), error: %s", targetPath, err)
+	}
+	defer func() {
+		if err := outFile.Close(); err != nil {
+			log.Warnf("Failed to close (%s)", targetPath)
+		}
+	}()
+
+	resp, err := http.Get(downloadURL)
+	if err != nil {
+		return fmt.Errorf("failed to download from (%s), error: %s", downloadURL, err)
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.Warnf("failed to close (%s) body", downloadURL)
+		}
+	}()
+
+	_, err = io.Copy(outFile, resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to download from (%s), error: %s", downloadURL, err)
+	}
+
+	return nil
+}
+
 
 // ListBuckets returns a slice of all the buckets for a given project.
 func ListBuckets(projectID string) ([]*storage.Bucket, error) {
@@ -61,7 +71,25 @@ func ListBuckets(projectID string) ([]*storage.Bucket, error) {
 }
 
 func main() {
-	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", os.Getenv("service_account_json_key_path"))
+	key_path := os.Getenv("service_account_json_key_path")
+
+	if strings.HasPrefix(key_path, "http") {
+		downloadUrl := key_path
+		tmpDir, err := pathutil.NormalizedOSTempDirPath("__google-cloud-storage__")
+
+		if err != nil {
+		  failf("Failed to create tmp dir, error: %s", err)
+		}
+
+		targetPath := filepath.Join(tmpDir, "key.json")
+		if err := downloadFile(key_path, targetPath); err != nil {
+				failf("Failed to download json key file, error: %s", err)
+		}
+
+		key_path = targetPath
+	}
+
+	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", key_path)
 
 	if len(os.Args) < 2 {
 		fmt.Println("usage: listbuckets <projectID>")
