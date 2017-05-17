bbb8ec25d771a3cfec9e3fc511d7c7a18cb56360
Vishal Banthia <vishal.banthia.vb@gmail.com>
2017-05-16 20:59:40 -0700

    add go dependency

--- step.sh
+++ step.sh
@@ -5,5 +5,6 @@ THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${THIS_SCRIPT_DIR}"
 
 go get -u cloud.google.com/go/storage
+go get -u -d github.com/bitrise-io/go-utils/pathutil
 
 go run ./gcs_upload.go ${project_name}
